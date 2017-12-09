6e9d5a32c6de0a111061b9b4fe19db80c5ca8fa9
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-12-09 16:19:06 -0500

    Completed day one with part_two

--- src/first_day.cr
+++ src/first_day.cr
@@ -5,11 +5,28 @@ module AdventOfCode
       sum = 0
 
       chars.each_with_index do |char, index|
-        i = char.to_i
+        val = char.to_i
         begin
-          sum += i if i == chars[index + 1].to_i
+          sum += val if val == chars[index + 1].to_i
         rescue IndexError
-          sum += i if i == chars.first.to_i
+          sum += val if val == chars.first.to_i
+        end
+      end
+
+      sum
+    end
+
+    def self.part_two(captcha)
+      chars = captcha.chars
+      half = chars.size/2
+      sum = 0
+
+      chars.each_with_index do |char, index|
+        val = char.to_i
+        begin
+          sum += val if val == chars[half + index].to_i
+        rescue IndexError
+          sum += val if val == chars[index - half].to_i
         end
       end
 
