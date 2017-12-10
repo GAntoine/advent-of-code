201c7383d3e7e5b45d0b99c951ad9f49a7319df2
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-12-09 19:02:21 -0500

    Completed part one of day three

---
+++ spec/third_day_spec.cr
@@ -0,0 +1,22 @@
+require "./spec_helper"
+require "../src/third_day"
+
+describe AdventOfCode::ThirdDay do
+  square = 368078
+
+  describe "#part_one" do
+    it "sums of all digits that match the next digit in the list" do
+      steps = AdventOfCode::ThirdDay.part_one(square)
+
+      steps.should eq(371)
+    end
+  end
+
+  describe "#part_two" do
+    it "sums of all digits that match the digit halfway around" do
+      steps = AdventOfCode::ThirdDay.part_two(square)
+
+      steps.should eq(369601)
+    end
+  end
+end
---
+++ src/third_day.cr
@@ -0,0 +1,20 @@
+module AdventOfCode
+  class ThirdDay
+    def self.part_one(square)
+      circle = (Math.sqrt(square) / 2 + 1).round
+      side_lenght = circle - 1 + circle
+      half_length = side_lenght - 1
+      diagonal = side_lenght ** 2
+      difference = diagonal - square
+
+      sides = (difference / half_length).to_i
+      movement = difference - (half_length * sides)
+      distance = movement - half_length / 2
+
+      (circle - 1 + distance.abs).to_i
+    end
+
+    def self.part_two(square)
+    end
+  end
+end
