e028a0a7fa1b02c7b24657ad59b2509aa29050a1
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-12-10 12:31:24 -0500

    Completed both parts of day 5

--- spec/day_5_spec.cr
+++ spec/day_5_spec.cr
@@ -2,19 +2,27 @@ require "./spec_helper"
 require "../src/day_5"
 
 describe AdventOfCode::FifthDay do
+  file_path = File.join(
+    [
+      File.dirname(__FILE__),
+      "data",
+      "day_5_input.txt",
+    ]
+  )
+
   describe "#part_one" do
     it "returns the number of steps to reach the exit" do
-      sum = AdventOfCode::FifthDay.part_one
+      jumps = AdventOfCode::FifthDay.part_one(file_path)
 
-      sum.should eq(0)
+      jumps.should eq(372139)
     end
   end
 
   describe "#part_two" do
     it "sums of all digits that match the digit halfway around" do
-      sum = AdventOfCode::FifthDay.part_two
+      jumps = AdventOfCode::FifthDay.part_two(file_path)
 
-      sum.should eq(0)
+      jumps.should eq(29629538)
     end
   end
 end
--- src/day_5.cr
+++ src/day_5.cr
@@ -1,9 +1,29 @@
 module AdventOfCode
   class FifthDay
-    def self.part_one
+    def self.part_one(file_path)
+      keep_jumping(file_path) { 1 }
     end
 
-    def self.part_two
+    def self.part_two(file_path)
+      keep_jumping(file_path) { |val| val >= 3 ? -1 : 1 }
+    end
+
+    def self.keep_jumping(file_path)
+      list = File.read_lines(file_path).map { |l| l.to_i }
+      jumps, pointer = 0, 0
+
+      while true
+        begin
+          val = list[pointer]
+          list[pointer] += yield val
+          pointer += val
+          jumps += 1
+        rescue IndexError
+          break
+        end
+      end
+
+      jumps
     end
   end
 end
