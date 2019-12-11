e5ed448f5fbbeb86298d507875d12172a9020967
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2019-12-10 20:51:20 -0800

    Complete day 10

--- src/2019/day_10.rb
+++ src/2019/day_10.rb
@@ -1,12 +1,44 @@
 require 'matrix'
+require 'set'
 
 space = Matrix[*File.readlines("./data/day_10.txt").map { |s| s.split("")[0..-2] }]
 
-space.each_with_index.map do |x, row, col|
-	count = 0
-	space.each_with_index.map do |y, subrow, subcol|
-		continue if row == subrow && col == subcol
+best = [0, 0]
+most = 0
 
-		horizontal = [col..subcol]
+(0..10).each do |x|
+	(0..10).each do |y|
+		next if space[x, y] != "#"
+
+		positive = Set[]
+		negative = Set[]
+
+		(0..10).each do |tx|
+			(0..10).each do |ty|
+				next if space[tx, ty] != "#"
+				next if ty === y && tx === x
+
+				begin
+					g = (tx - x) / (ty - y)
+				rescue ZeroDivisionError => e
+					g = Float::INFINITY
+				end
+
+				if tx > x || (tx === x && ty > y)
+					positive.add(g)
+				else
+					negative.add(g)
+				end
+			end
+		end
+
+		n = positive.size + negative.size;
+		if n > most
+			most = n
+			best = [x, y]
+
+		end
 	end
 end
+
+puts most
